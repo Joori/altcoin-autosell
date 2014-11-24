5e97b2cd41121bf6e3b004f79eced2adcea5c7d5
Jerry Linder <jerryl1089@gmail.com>
2014-11-24 11:16:51 -0600

    Added _RefreshMarkets() function that adds the max price seen to the markets it initializes to use in the _MarketRefreshLoop() function.

--- cryptsy_api.py
+++ cryptsy_api.py
@@ -4,7 +4,7 @@ import hashlib
 import hmac
 import json
 import time
-#import thread
+import thread
 
 try:
     import http.client
@@ -104,10 +104,25 @@ class Cryptsy(exchange_api.Exchange):
                     market['primary_currency_code'], market['marketid'], True, 1/market['lasttradeprice'])
             self._markets[market2.GetSourceCurrency()][market2.GetTargetCurrency()] = market2
 
+    def _RefreshMarkets(self):
+        for market in self._Request('getmarkets')['return']:
+            max_price_seen = self._markets[market['primary_currency_code']][market['secondary_currency_code']].GetLastPriceSeen()
+            last_trade_price = market['lasttradeprice']
+            market1 = Market(self, market['primary_currency_code'],
+                    market['secondary_currency_code'], market['marketid'], False, last_trade_price,
+                    last_trade_price if last_trade_price > max_price_seen else max_price_seen)
+            self._markets[market1.GetSourceCurrency()][market1.GetTargetCurrency()] = market1
+            max_price_seen = self._markets[market['secondary_currency_code']][market['primary_currency_code']].GetLastPriceSeen()
+            last_trade_price = 1/market['lasttradeprice']
+            market2 = Market(self, market['secondary_currency_code'],
+                    market['primary_currency_code'], market['marketid'], True, last_trade_price,
+                    last_trade_price if last_trade_price > max_price_seen else max_price_seen)
+            self._markets[market2.GetSourceCurrency()][market2.GetTargetCurrency()] = market2
+
     def _MarketRefreshLoop(self):
         while True:
             time.sleep(5)
-            self._LoadMarkets()
+            self._RefreshMarkets()
 
     def _Request(self, method, post_dict=None):
         if post_dict is None:
